f11cb17bac83ae6eec001d79f59a9763fdb458af
HannaNikola <h.nikolaienko32@gmail.com>
2023-08-28 23:59:48 +0200

    upgrate

--- src/index.js
+++ src/index.js
@@ -1,98 +1,92 @@
-import axios from 'axios';
+// import axios from 'axios';
 import NewsApiService from './cat-api';
+import SlimSelect from 'slim-select';
+
+
 
 const selectorType = document.querySelector('.breed-select');
 const loaderElement = document.querySelector('.loader');
-const ErrorElement = document.querySelector('.error');
+const errorElement = document.querySelector('.error');
 const infoBoxe = document.querySelector('.cat-info');
 
-console.log(selectorType);
-console.log(loaderElement);
-console.log(ErrorElement);
-console.log(infoBoxe);
-
 const searchOptions = {
-    headers: { 'x-api-key': 'live_gXfdXzaFDcewDJeq5HVifu2LVbBRmWM5pCaSZvmHUfx80lvNoLZkqGvP1sdopeeW' },
-}
-
+  headers: {
+    'x-api-key':
+      'live_gXfdXzaFDcewDJeq5HVifu2LVbBRmWM5pCaSZvmHUfx80lvNoLZkqGvP1sdopeeW',
+  },
+};
 
 function fetchBreeds() {
-    // const params = new URLSearchParams({
-    //     api_key:
-    //         'live_gXfdXzaFDcewDJeq5HVifu2LVbBRmWM5pCaSZvmHUfx80lvNoLZkqGvP1sdopeeW',
-    // });
-
-    return fetch(
-        `https://api.thecatapi.com/v1/breeds?x-api-key=${searchOptions.headers}`
-    ).then(resp => {
-        if (!resp.ok) {
-            throw new Error('Error');
-        }
-        return resp.json();
-    });
+ return fetch(
+    `https://api.thecatapi.com/v1/breeds?x-api-key=${searchOptions.headers}`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error('Error');
+    }
+    return resp.json();
+  });
 }
 
 fetchBreeds()
-    .then(data => {
-        // console.log(data);
+  .then(data => {
+   
 
-        
-        selectorType.insertAdjacentHTML('beforeend',createSelectBread(data));
-    })
-    .catch(ero => {});
-
+    selectorType.insertAdjacentHTML('beforeend', createSelectBread(data));
+  })
+    .catch(ero => {
+        loaderElement.style.display = 'block';
+        errorElement.style.display = 'none';
+  });
 
 function createSelectBread(arr) {
-    return arr
-        .map(({ id,name }) =>  `<option value="${id}">${name}</option>`).join(" ");
-
+return arr
+    .map(
+      ({ reference_image_id, name }) =>
+        `<option value="${reference_image_id}">${name}</option>`
+    )
+    .join(' ');
 }
-    
 
 // Другий запит
 
 function fetchCatByBreed(breedId) {
-    
-    // const apiKey = 'live_gXfdXzaFDcewDJeq5HVifu2LVbBRmWM5pCaSZvmHUfx80lvNoLZkqGvP1sdopeeW'
-    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api-key=${searchOptions.headers}`
-    console.log(breedId)
+ 
+  const url = `https://api.thecatapi.com/v1/images/${breedId}`;
 
-    return fetch(url).then(resp => {
-
-        if (!resp.ok) {
-            throw new Error('Error');
-        }
-        return resp.json();
-        
-    });
-
+  return fetch(url).then(resp => {
+    if (!resp.ok) {
+      throw new Error('Error');
+    }
+    return resp.json();
+  });
 }
 
-selectorType.addEventListener('change', (event) => {
-    const selectedBreedId = event.target.value;
-    console.log(event.target)
-    fetchCatByBreed(selectedBreedId).then(
-        data => {
-            console.log(data)
-
-            infoBoxe.innerHTML = createCatInfo(data);
-        }
-    ).catch(ero => { });
+selectorType.addEventListener('change', event => {
+    loaderElement.style.display = 'block';
+    errorElement.style.display = 'none';
+  const selectedBreedId = event.target.value;
+    fetchCatByBreed(selectedBreedId)
+      
+        .then(data => {
+          infoBoxe.innerHTML = createCatInfo(data);
+        })
+        .catch(ero => {
+          console.log('error');
+          loaderElement.style.display = 'none';
+          errorElement.style.display = 'block';
+        });
 });
 
-
-function createCatInfo(data) { 
-    // console.log(data)
-    return data.map(({ reference_image_id, name, description, temperament }) => 
-        
-    `<img src="" alt="">
-    <h1>${name}</h1>
-    <p>${description}</p>
-    <p>${temperament}</p>`).join(' ');
-    
+function createCatInfo(data) {
+  console.log(data);
+  loaderElement.style.display = 'none';
+  const markup = `<img src=${data.url} alt=cat width= ${
+    data.width / 3
+  } height= ${data.height / 3} >
+   <h1>${data.breeds[0].name}</h1>
+   <p>${data.breeds[0].description}</p>
+   <p> <b>Temperament: </b>${data.breeds[0].temperament}</p>
+  `;
+  return markup;
 }
-
-
-
-    
 
